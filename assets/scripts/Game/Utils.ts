28e9ad44486820d592c7ba5f2f6fccef15de2e52
xizheng <xizheng.shi@digitalbrain.cn>
2023-02-22 23:40:58 +0800

    chore: dev gameworld

--- assets/scripts/Game/Utils.ts
+++ assets/scripts/Game/Utils.ts
@@ -1,4 +1,27 @@
+// @ts-nocheck
+
 // 将给定数字调整为8的倍数
 export function adjustNumber(number: number) {
-    return Math.round(number / 8) * 8;
+  return Math.round(number / 8) * 8;
+}
+
+export function world(_game) {
+  const _mapLayer = _game.mapLayer;
+  const pause = () => {
+    cc.game.pause();
+  };
+  const resume = () => {
+    cc.game.resume();
+  };
+  const start = (level = 1) => {
+    _game.level = level;
+    _game.gameStart();
+  };
+
+  window.__gameworld__ = {
+    _game,
+    pause,
+    resume,
+    start,
+  };
 }
