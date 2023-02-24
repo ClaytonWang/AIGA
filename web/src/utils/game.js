1310ccebdc016db21dcff435f7e773aeab9b859f
xizheng <xizheng.shi@digitalbrain.cn>
2023-02-24 10:35:34 +0800

    chore: 新增地图转换func

--- web/src/utils/game.js
+++ web/src/utils/game.js
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 let inited = false;
 const init = () => {
   if (!inited) {
@@ -6,6 +8,36 @@ const init = () => {
       console.error(err);
     });
   }
+};
+
+// 格式化map数据,for game
+const encodeMap = (mapData) => {
+  // 0. 预处理数据
+  const mapStr = (() => {
+    return _.replace(
+      _.isArray(mapData) ? mapData.join("") : _.toString(mapData),
+      /\s+/g,
+      ""
+    );
+  })();
+  // 1.填充
+  return _.padStart(mapStr, 26 * 26, "0");
+};
+
+// 格式化map数据,for display
+const decodeMap = (mapStr) => {
+  // 0. 预处理数据
+  const data = _.replace(mapStr, /\s+/g, "");
+  // 1. 26*26空地图
+  const standard = (() => {
+    return _.map(new Array(26).fill(1), () => _.padStart("", 26, "0"));
+  })();
+  // 2. 根据data填充地图
+  for (let i = 0; i * 26 < data.length; i++) {
+    const str = _.padStart(data.slice(i * 26, (i + 1) * 26), 26, "0");
+    standard[i] = str;
+  }
+  return standard.join("\n");
 };
 
 const game = (() => {
