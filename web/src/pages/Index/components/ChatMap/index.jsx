dbb6463ee15aac0da0080dd9fd4cbb2e3b449b0f
xizheng <xizheng.shi@digitalbrain.cn>
2023-02-28 20:52:35 +0800

    chore: 调整聊天组件UI

--- web/src/pages/Index/components/ChatMap/index.jsx
+++ web/src/pages/Index/components/ChatMap/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Chat, { Bubble, useMessages } from "@chatui/core";
 import "@chatui/core/dist/index.css";
 import "./index.less";
@@ -6,15 +6,9 @@ import "./index.less";
 const initialMessages = [
   {
     type: "text",
-    content: { text: "主人好，我是智能助理，你的贴心小助手~" },
+    content: { text: "你好，我是AI创游，你的游戏创作助手~" },
     user: {
-      avatar: "//gw.alicdn.com/tfs/TB1DYHLwMHqK1RjSZFEXXcGMXXa-56-62.svg",
-    },
-  },
-  {
-    type: "image",
-    content: {
-      picUrl: "//img.alicdn.com/tfs/TB1p_nirYr1gK0jSZR0XXbP8XXa-300-300.png",
+      avatar: "/image/logo.png",
     },
   },
 ];
@@ -22,21 +16,18 @@ const initialMessages = [
 // 默认快捷短语，可选
 const defaultQuickReplies = [
   {
-    icon: "message",
-    name: "联系人工服务",
+    name: "开始游戏",
     isNew: true,
     isHighlight: true,
+    text: "",
   },
   {
-    name: "短语1",
-    isNew: true,
+    name: "随机关卡",
+    text: "",
   },
   {
-    name: "短语2",
-    isHighlight: true,
-  },
-  {
-    name: "短语3",
+    name: "AI关卡",
+    text: "",
   },
 ];
 
@@ -44,8 +35,8 @@ const ChatMap = () => {
   // 消息列表
   const { messages, appendMsg, setTyping } = useMessages(initialMessages);
 
-  // 发送回调
-  function handleSend(type, val) {
+  // 用户发送信息
+  const handleUserSend = useCallback((type, val) => {
     if (type === "text" && val.trim()) {
       // TODO: 发送请求
       appendMsg({
@@ -64,14 +55,14 @@ const ChatMap = () => {
         });
       }, 1000);
     }
-  }
+  }, []);
+  // 快捷操作
+  const handleQuickReplyClick = useCallback((item) => {
+    handleUserSend("text", item.text || item.name);
+  }, []);
 
-  // 快捷短语回调，可根据 item 数据做出不同的操作，这里以发送文本消息为例
-  function handleQuickReplyClick(item) {
-    handleSend("text", item.name);
-  }
-
-  function renderMessageContent(msg) {
+  // 渲染消息体
+  const renderMessageContent = useCallback((msg) => {
     const { type, content } = msg;
 
     // 根据消息类型来渲染
@@ -87,17 +78,17 @@ const ChatMap = () => {
       default:
         return null;
     }
-  }
+  }, []);
 
   return (
     <div className="chat-map-zone">
       <Chat
-        navbar={{ title: "智能助理" }}
+        navbar={{ title: "AI创游" }}
         messages={messages}
         renderMessageContent={renderMessageContent}
         quickReplies={defaultQuickReplies}
         onQuickReplyClick={handleQuickReplyClick}
-        onSend={handleSend}
+        onSend={handleUserSend}
       />
     </div>
   );
